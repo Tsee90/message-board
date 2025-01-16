ce04379a887249760f79735cb266415568b6a5a7
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2025-01-16 15:56:38 -0500

    Create mini message board

---
+++ routes/indexRouter.js
@@ -0,0 +1,26 @@
+const { Router } = require('express');
+const indexRouter = Router();
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+indexRouter.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+indexRouter.post('/new', (req, res) => {
+  const { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = indexRouter;
