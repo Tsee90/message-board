f9c89ab58a6823a5eed77c45b8e4b4a906e9e64d
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2025-01-20 14:50:38 -0500

    Fix bug attempt

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('node:path');
 const indexRouter = require('./routes/indexRouter');
 const newRouter = require('./routes/newRouter');
 const { body, validationResult } = require('express-validator');
+
+const dbenv = require('./db/dbenv');
+dbenv.init();
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 app.use('/new', newRouter);
 
-const PORT = 5432;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Message Board - listening on port ${PORT}!`);
 });
