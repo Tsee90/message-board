ccdaf2ebb5e6dc511c6e8db49016ecfd2a121df3
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2025-01-20 16:25:48 -0500

    Please wtf?

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  connectionString: '{{Postgres.DATABASE_URL}}' || process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
